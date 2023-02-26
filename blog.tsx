8658883b82eabd791bafc2528a893f1b259abe17
RanaRabees <adeemaamir1722@gmail.com>
2023-02-26 10:32:55 +0500

    d
    d

---
+++ blog.tsx
@@ -0,0 +1,205 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react'
+import '../app/globals.css'
+import '../app/page.module.css'
+
+function blog() {
+    return (
+        <>
+
+            <div>
+                <nav>
+                    <ul className='bg-red-600 w-full h-40 sm:w-fit sm:h'>
+                        <center><h1 className='text-7xl font-extrabold text-yellow-200'>Ahtasham Daig Services</h1></center>
+                    </ul>
+
+                    <ul className='bg-black w-full h-32'>
+                        <li>
+
+                        </li>
+                    </ul>
+
+                    <ul className='bg-green-400 w-full h-32'>
+                        <li>
+
+                        </li>
+                    </ul>
+
+                    <ul className='bg-blue-700 w-full h-32'>
+                        <li>
+
+                        </li>
+                    </ul>          </nav>
+            </div>
+
+            <center><h1 className='text-6xl font-mono text-orange-700 '>Ahtasham Online Daig Services</h1></center>
+            <br /><br /><br /><br /><br /><br /><br /><br />
+            <br /><br /><br /><br /><br /><br /><br /><br />
+
+
+
+            <h1 className='text-4xl font-mono relative bottom-52 left-10 text-red-500'>Variety of Dishes
+                <img className='relative bottom-10 left-96' src="/c2.png" alt="image" />
+            </h1>
+            <p className='text-red-600 relative bottom-64 left-10'>Daig’s tasteful menus can <br /> be found in different dishes<br /> according to your preferences.<br />
+                <div className="flex justify-evenly">
+                    <img src="/1.jpg" className='w-72 h-72' alt="jpg" />
+                    <img src="/2.jpg" className='w-72 h-72' alt="jpg" />
+                    <img src="/3.png" className='w-72 h-72' alt="jpg" />
+                    <img src="/4.jpg" className='w-72 h-72' alt="jpg" />
+                </div>
+            </p>
+            <h1 className='text-4xl font-mono relative -top-60 left-10 text-red-500'>Catering
+
+            </h1>
+
+
+            <br /><br /><br /><br /><br /><br /><br /><br />
+            <br /><br /><br /><br /><br /><br /><br /><br />
+
+
+
+            <h1 className='text-4xl font-mono relative bottom-52 left-10 text-red-500'>Variety of Dishes
+                <img className='relative bottom-10 left-96' src="/c2.png" alt="image" />
+            </h1>
+            <p className='text-red-600 relative bottom-64 left-10'>Daig’s tasteful menus can <br /> be found in different dishes<br /> according to your preferences.<br />
+                <div className="flex justify-evenly">
+                    <img src="/1.jpg" className='w-72 h-72' alt="jpg" />
+                    <img src="/2.jpg" className='w-72 h-72' alt="jpg" />
+                    <img src="/3.png" className='w-72 h-72' alt="jpg" />
+                    <img src="/4.jpg" className='w-72 h-72' alt="jpg" />
+                </div>
+            </p>
+            <h1 className='text-4xl font-mono relative -top-60 left-10 text-red-500'>Catering
+
+            </h1>
+
+            <br /><br /><br /><br /><br /><br /><br /><br />
+            <br /><br /><br /><br /><br /><br /><br /><br />
+
+
+
+            <h1 className='text-4xl font-mono relative bottom-52 left-10 text-red-500'>Variety of Dishes
+                <img className='relative bottom-10 left-96' src="/c2.png" alt="image" />
+            </h1>
+            <p className='text-red-600 relative bottom-64 left-10'>Daig’s tasteful menus can <br /> be found in different dishes<br /> according to your preferences.<br />
+                <div className="flex justify-evenly">
+                    <img src="/1.jpg" className='w-72 h-72' alt="jpg" />
+                    <img src="/2.jpg" className='w-72 h-72' alt="jpg" />
+                    <img src="/3.png" className='w-72 h-72' alt="jpg" />
+                    <img src="/4.jpg" className='w-72 h-72' alt="jpg" />
+                </div>
+            </p>
+            <h1 className='text-4xl font-mono relative -top-60 left-10 text-red-500'>Catering
+                {/* <br /> <br /> */}
+
+            </h1>
+            <div className="flex justify-evenly">
+                <img src="/5.jpg" className='w-72 h-72' alt="jpg" />
+                <img src="/6.jpg" className='w-72 h-72' alt="jpg" />
+                <img src="/7.webp" className='w-72 h-72' alt="jpg" />
+                <img src="/9.jpeg" className='w-72 h-72' alt="jpg" />
+            </div>
+            <p className='text-red-600 relative '>Daig provides good food<br />services along with waiters’<br />crew and all sort of catering<br />arrangement.<br />
+            </p>
+
+
+            {/* </p> */}
+            <br />
+
+            br
+            <h1 className='text-4xl font-mono relative bottom-60 left-10 text-red-500'>Sadqa Daig
+                <img className='relative bottom-10 left-96' src="/c3.png" alt="image" /> </h1>
+            <p className='text-red-600 relative bottom-72 left-10'>We present you the easiest<br />way to offer your charity in<br />form of Sadqah Daig.<br />
+
+            </p>
+
+
+
+            <h1 className='text-4xl font-mono relative bottom-64 left-10 text-red-500'>Weddings & Events
+                <img className='relative bottom-10 left-96' src="/c4.webp" alt="image" /> </h1>
+            <p className='text-red-600 relative bottom-72 left-10'>For Weddings and Events,<br />we provide the service of<br />ideal arrangments and food<br />refreshments.<br />
+
+            </p>
+            {
+            /* <span className="inline-grid grid-cols-3 w-full h-96 gap-4">
+                <span className="bg-[url('/1.jpg')] bg-no-repeat bg-cover">01</span>
+                <span className="bg-[url('/2.jpg')] bg-no-repeat bg-cover">02</span>
+                <span className="bg-[url('/3.png')] bg-no-repeat bg-cover">03</span>
+                <span className="bg-[url('/4.jpg')] bg-no-repeat bg-cover">04</span>
+                <span className="bg-[url('/5.jpg')] bg-no-repeat bg-cover">05</span>
+                <span className="bg-[url('/6.jpg')] bg-no-repeat bg-cover">06</span>
+            </span>
+            <span className="inline-grid grid-cols-3 w-full h-96 gap-4">
+                <span className="bg-[url('/7.webp')] bg-no-repeat bg-cover">02</span>
+                <span className="bg-[url('/12.jpeg')] bg-no-repeat bg-cover">03</span>
+                <span className="bg-[url('/9.jpeg')] bg-no-repeat bg-cover">04</span>
+                <span className="bg-[url('/6.jpg')] bg-no-repeat bg-cover">05</span>
+                <span className="bg-[url('/10.webp')] bg-no-repeat bg-cover">06</span>
+                <span className="bg-[url('/11.webp')] bg-no-repeat bg-cover">01</span>
+            </span> */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <div className='bg-red-300'>
+                <div className='bg-red-400'>
+
+                </div>
+            </div>
+
+
+            <h1 className='text-3xl font-mono text-red-500 relative left-10'>Tents & Arrangements
+                <img className='relative bottom-10 left-96' src="https://cdn.shopify.com/s/files/1/0436/7371/4851/files/tents-icon_60x60.png?v=1636984440" alt="image" /> </h1>
+            <p className='text-red-600 relative bottom-11 left-10'>Daig set up your events and<br />ceremonies by providing the<br />services of tents, crockery,<br />and many more.<br />
+
+            </p>
+
+            <h1 className='text-4xl font-mono relative left-10 text-red-500'>Fast Delivery
+                <img className='relative bottom-10 left-96' src="https://cdn.shopify.com/s/files/1/0436/7371/4851/files/fast-delivery-icon_60x60.png?v=1636984457" alt="image" /> </h1>
+
+            <p className='text-red-600 relative bottom-11 left-10'>Daig provides the online<br />daig delivering service in the<br />fastest and hassle-free<br />manner.<br />
+
+            </p>
+
+            {/* <span className="inline-grid grid-cols-2 w-full h- gap-4" id='panagrid'>
+                <span className="bg-slate-500 w-full"><img className='relative top-20' src="/html.png" alt="html" /><p className='text-4xl relative left-96 bottom-20'>this is daig</p></span>
+                <span className="bg-green-500 w-full">02</span>
+                <span className="bg-red-500 w-full">03</span>
+                <span className="bg-blue-500 w-full">04</span>
+                <span className="bg-yellow-500 w-full">05</span>
+                <span className="bg-pink-500 w-full">06</span>
+            </span> */}
+
+
+
+            {/* Cards on first page */}
+
+
+
+            <br />
+            <br />
+            <br />
+
+            <div className="flex justify-evenly">
+                <img src="/5.jpg" className='w-72 h-72' alt="jpg" />
+                <img src="/6.jpg" className='w-72 h-72' alt="jpg" />
+                <img src="/7.webp" className='w-72 h-72' alt="jpg" />
+                <img src="/9.jpeg" className='w-72 h-72' alt="jpg" />
+            </div>
+        </>
+    )
+}
+
+export default blog;
